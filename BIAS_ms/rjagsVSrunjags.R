7693c0708a7756501e12a594137fb573e87d40a2
Pulkkinen Henni (LUKE) <henni.pulkkinen@luke.fi>
2026-02-10 10:51:14 +0200

    read in data

--- BIAS_ms/rjagsVSrunjags.R
+++ BIAS_ms/rjagsVSrunjags.R
@@ -1,6 +1,10 @@
 
-test<-model{x~dnorm(0,1)}
-cat(BIAS,file="test.txt")
+# rjags: sample= n.iter/thin
+# runjags: sample= sample
+
+
+test<-"model{x~dnorm(0,1)}"
+cat(test,file="test.txt")
 
 jm<-jags.model('test.txt', n.adapt=1000, n.chains=1)
 
@@ -11,4 +15,24 @@ system.time(chains2<-coda.samples(jm,
                                   n.iter=600000,
                                   thin=500))
 
-save()
+run0<-run.jags(test, monitor=c("x"),n.chains = 1, thin=500,
+         sample =600000, adapt = 1000,
+         keep.jags.files=F,
+         progress.bar=TRUE, jags.refresh=100)
+
+
+length(chains2[[1]]) # 1200
+
+
+parnames<-c(
+  "x")
+
+run0<- run.jags(test, monitor= parnames,
+                     #data=datalist,
+                     n.chains = 2, method = 'parallel', thin=500,
+                     burnin =10, modules = "mix",
+                     sample =600000, adapt = 1000,
+                     keep.jags.files=F,
+                     progress.bar=TRUE, jags.refresh=100)
+
+chains<-as.mcmc(run0)

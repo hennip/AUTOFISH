ac8a13c8c15ffa90e0f1fde2722d1d3209d22294
Pulkkinen Henni (LUKE) <henni.pulkkinen@luke.fi>
2026-02-19 15:15:51 +0200

    wip

--- data/read-in-trawl-data.R
+++ data/read-in-trawl-data.R
@@ -131,8 +131,8 @@ for(r in 1:4){
 Hprops
 
 
-# Lobs[1:8,r,s,y]: Number of fish of species s in all haul samples at rectangle r from length groups 1:8
 # nLobs[r,s,y]: Total sample size per rectangle and species
+# Lobs[1:8,r,s,y]: Number of fish of species s in all haul samples at rectangle r from length groups 1:8
 #######################################################################################
 # nLobs
 #==========================
@@ -143,9 +143,10 @@ sample_size<-dfB_catch  |>
 sample_size
 
 # Sample size per species and rec in a form that feeds to the model
-num_Lobs<-array(NA, dim=c(4,2))
-num_Lobs<-as.data.frame(sample_size |> 
+nL_obs<-array(NA, dim=c(4,2))
+nL_obs<-as.data.frame(sample_size |> 
             pivot_wider(values_from = tot_sample, names_from = species))[,2:3]
+nL_obs
 
 # Lobs
 #==========================
@@ -158,42 +159,38 @@ dfB_catch |>group_by(CatchSpeciesCode) |>
 # 8th group
 length_limits<-c(90,105,120,135,150,165,180)
 
-# Number of herring in the sample per 
+# Number of herring/other species in the sample per rectangle and length group
 numbers_at_length<-dfB_catch|>
   group_by(rec_ruhne, species, CatchLengthClass)|>  
   summarise(n=sum(CatchNumberAtLength)) |> 
-  mutate(length_group=ifelse(CatchLengthClass<90, 1, NA)) |> 
-  mutate(length_group=ifelse(CatchLengthClass>=90 & CatchLengthClass<105, 2, length_group)) |> 
-  mutate(length_group=ifelse(CatchLengthClass>=105 & CatchLengthClass<120, 3, length_group)) |> 
-  mutate(length_group=ifelse(CatchLengthClass>=120 & CatchLengthClass<135, 4, length_group)) |> 
-  mutate(length_group=ifelse(CatchLengthClass>=135 & CatchLengthClass<150, 5, length_group)) |> 
-  mutate(length_group=ifelse(CatchLengthClass>=150 & CatchLengthClass<165, 6, length_group)) |> 
-  mutate(length_group=ifelse(CatchLengthClass>=165 & CatchLengthClass<180, 7, length_group)) |> 
-  mutate(length_group=ifelse(CatchLengthClass>=180, 8, length_group)) |> 
+  mutate(LengthClass=CatchLengthClass)
+  mutate(length_group=ifelse(LengthClass<90, 1, NA)) |> 
+  mutate(length_group=ifelse(LengthClass>=90  & LengthClass<105, 2, length_group)) |> 
+  mutate(length_group=ifelse(LengthClass>=105 & LengthClass<120, 3, length_group)) |> 
+  mutate(length_group=ifelse(LengthClass>=120 & LengthClass<135, 4, length_group)) |> 
+  mutate(length_group=ifelse(LengthClass>=135 & LengthClass<150, 5, length_group)) |> 
+  mutate(length_group=ifelse(LengthClass>=150 & LengthClass<165, 6, length_group)) |> 
+  mutate(length_group=ifelse(LengthClass>=165 & LengthClass<180, 7, length_group)) |> 
+  mutate(length_group=ifelse(LengthClass>=180, 8, length_group)) |> 
   group_by(rec_ruhne, species, length_group) |> 
   summarise(number_at_length=sum(n)) |> 
-  pivot_wider(names_from = rec_ruhne, values_from=number_at_length)
+  pivot_wider(names_from = rec_ruhne, values_from=number_at_length) |> 
+  arrange(species, length_group)
   
 print(n=50, x=numbers_at_length)
 
-
-
-# number_at_length<-dfB_catch |> 
-#   #group_by(species, CatchLengthClass ) |> 
-#   group_by(rec_ruhne, species, CatchLengthClass ) |> 
-#   summarise(N_at_length=sum(CatchNumberAtLength))
-# number_at_length
-
-#View(number_at_length)
-
-
-
+# Sample size per species and rec in a form that feeds to the model
+L_obs<-array(NA, dim=c(8,4,2))
+for(r in 1:4){
+  L_obs[,r,1]<-as.data.frame(numbers_at_length |> filter(species==1) |> ungroup() |> select(-species, -length_group))[,r]
+  L_obs[,r,2]<-as.data.frame(numbers_at_length |> filter(species==2) |> ungroup() |> select(-species, -length_group))[,r]
+}
+L_obs
 
 
 ###############
-# Age data
+# Herring Age data
 ###############
-
 
 dfB_biol24<-read.csv(str_c(path,"Biotic_2024-ZR038_2025-03-12T10.40.08.950.csv"), skip=500) |> mutate(year=2024)|> as_tibble()
 dfB_biol23<-read.csv(str_c(path,"Biotic_2023-ZR055_2024-02-06T10.01.18.377.csv"), skip=485) |> mutate(year=2023)|> as_tibble()
@@ -212,5 +209,32 @@ print(n=35, x=df |> select(length, age) |> group_by(length, age) |> summarise(n=
   pivot_wider(names_from = age, values_from = n) |> 
     select(`0`,`1`,`2`,`3`,`4`,`5`,`6`,`7`, `8`,`9`,`10`,`11`,`12`,`13`,`14`,`NA`))
 
+# nGobs[l,r,y]: Age sample size 
+# Gobs[1:Nages,l,r,y]: Sample size on age samples from length group l
+# ===================================================================
 
+# Oho. Ikä unohtui. Jatketaan tästä...
 
+df<-dfB_biol24 |> select(-Biology, -Header, -CruiseLocalID, -HaulGear) |> 
+  filter(CatchSpeciesCode==126417) |> 
+  mutate(LengthClass=BiologyLengthClass) |> 
+  mutate(length_group=ifelse(LengthClass<90, 1, NA)) |> 
+  mutate(length_group=ifelse(LengthClass>=90  & LengthClass<105, 2, length_group)) |> 
+  mutate(length_group=ifelse(LengthClass>=105 & LengthClass<120, 3, length_group)) |> 
+  mutate(length_group=ifelse(LengthClass>=120 & LengthClass<135, 4, length_group)) |> 
+  mutate(length_group=ifelse(LengthClass>=135 & LengthClass<150, 5, length_group)) |> 
+  mutate(length_group=ifelse(LengthClass>=150 & LengthClass<165, 6, length_group)) |> 
+  mutate(length_group=ifelse(LengthClass>=165 & LengthClass<180, 7, length_group)) |> 
+  mutate(length_group=ifelse(LengthClass>=180, 8, length_group)) |> 
+  full_join(df_rec) |> 
+  select(rec_ruhne, HaulNumber,length_group, BiologyIndividualAge, year)
+df
+#View(df)
+
+# Tämä voi olla ok, ->Gobs
+df2<-as.data.frame(df |> group_by(rec_ruhne, length_group) |> summarise(n=n()) |> 
+  pivot_wider(values_from = n, names_from = rec_ruhne) |> 
+    ungroup() |> select(-length_group))
+df2
+
+# Sitten tarvii Gobsin, jossa jokaiselle ruudulle oma age-length matriisi. Easy peasy?

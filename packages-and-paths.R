1b2acb1675d1bf85f8fa1b3391178e0a8ebed359
hennip <henni.pulkkinen@luke.fi>
2026-02-25 09:53:04 +0200

    paths

--- packages-and-paths.R
+++ packages-and-paths.R
@@ -2,4 +2,5 @@ library(readxl)
 library(tidyverse)
 library(forcats)
 
-path<-"../../01-Projects/AUTOFISH/dat/orig/"
+#path<-"../../01-Projects/AUTOFISH/dat/orig/"# laptop
+path<-"../../dat/AUTOFISH/orig/"# kala1

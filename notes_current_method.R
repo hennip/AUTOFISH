251c91fe3475a945be819d38887b8ba8e0ef2039
Pulkkinen Henni (LUKE) <henni.pulkkinen@luke.fi>
2026-03-16 12:19:58 +0200

    wip

--- notes_current_method.R
+++ notes_current_method.R
@@ -11,17 +11,23 @@ library(mapplots)
 # rect <- ices.rect2(lon, lat)
 # # rect
 
+# BIAS survey data for 2024 => change path
+pathA<-pathB<-"../../01-Projects/AUTOFISH/dat/BIAS_24/"
+dfA24<-read.csv(str_c(pathA,"Acoustic_ESTBIAS2024_2025-01-03T08.14.20.660.csv"), skip=11) |> as_tibble() |> mutate(year=2024)
+source("data/read-in-trawl-data.R") 
 
-dfA
+
+dfA24
 catch_all
 hauls_all
 bio_all
 
 choose_year<-2024
 
-df_acou<-dfA |> filter(year==choose_year)
+df_acou<-dfA24 |> filter(year==choose_year)
 df_catch<-catch_all|> filter(SurveyYear==choose_year) |> 
-  mutate(CatchSpeciesCategoryNumber=as.numeric(CatchSpeciesCategoryNumber))
+  mutate(CatchSpeciesCategoryNumber=as.numeric(CatchSpeciesCategoryNumber),
+         CatchNumberAtLength=as.numeric(CatchNumberAtLength))
 df_hauls<-hauls_all|> filter(SurveyYear==choose_year)
 df_bio<-bio_all|> filter(SurveyYear==choose_year)
 
@@ -34,40 +40,100 @@ df<-df_acou |> mutate(rec=ices.rect2(LogLongitude, LogLatitude)) |>
 # Take mean NASC per ICES rectangle
 df2<-df |> group_by(year, rec) |> 
   summarise(mean_edsu=mean(edsu))
+  df2
   
-  
-View(df)
-
-View(df_hauls)
-View(df_catch)
+# View(df)
+# 
+# View(df_hauls)
+# View(df_catch)
 
 df_haul_rec<-df_hauls |> select(SurveyYear, HaulNumber, HaulStatisticalRectangle)
 
-# persentage of species in the haul
+# Calculate percentage of each species in the haul
 
-df1<-df_catch |> left_join(df_haul_rec) |> select(HaulStatisticalRectangle, everything()) |> 
+df3<-df_catch |> left_join(df_haul_rec) |> select(HaulStatisticalRectangle, everything()) |> 
   select(CatchSpeciesCategoryNumber, CatchSpeciesCode, everything())
 
-df2<-df1 |> 
+df4<-df3 |> 
   select(CatchSpeciesCategoryNumber, CatchSpeciesCode, SurveyYear, HaulNumber,HaulStatisticalRectangle) |> 
   distinct() 
 
-tot_catch_per_haul<-df2|> 
+tot_catch_per_haul<-df4|> 
   group_by(SurveyYear, HaulNumber, HaulStatisticalRectangle) |> 
   summarise(tot_catch=sum(CatchSpeciesCategoryNumber))
 
-p_species_per_rectangle<-df2  |> left_join(tot_catch_per_haul) |> mutate(p_haul=CatchSpeciesCategoryNumber/tot_catch*100) |> 
+p_species_per_rectangle<-df4  |> left_join(tot_catch_per_haul) |> 
+  mutate(p_haul=CatchSpeciesCategoryNumber/tot_catch*100) |> 
   group_by(SurveyYear, CatchSpeciesCode,HaulStatisticalRectangle) |> 
   summarise(p_rec=mean(p_haul)) # NOTE! THIS GIVES EQUAL WEIGHTS FOR HAULS OF DIFFERENT SIZE!!!
-print(x=p_species_per_rectangle, n=50)
+print(x=p_species_per_rectangle, n=100)
 
 
-df2
+#p_per_length_per_haul<-
+  sample_size_per_length<-df3 |> 
+  group_by(SurveyYear, CatchSpeciesCode, HaulNumber) |> 
+  summarise(sample_size_per_length=sum(CatchNumberAtLength))
 
-View(df)
-  #select(CatchSpeciesCategoryNumber, HaulNumber, , everything())
+per_length<-df3 |> left_join(sample_size_per_length) |> 
+  mutate(p_per_length=CatchNumberAtLength/sample_size_per_length*100) |> 
+  select(p_per_length, CatchLengthClass,everything()) |> 
+  select(SurveyYear, HaulStatisticalRectangle, HaulNumber, everything()) |> 
+  mutate(CatchLengthClass=as.numeric(CatchLengthClass)) 
+
+# multiply the precentage of species at certain length per haul with the share 
+# of that species in the haul
+
+# percentage of species at certain length is 
+per_length
+
+# share of each species in the haul is
+df_p_species_per_haul<-df4  |> left_join(tot_catch_per_haul) |> 
+  mutate(p_species_per_haul=CatchSpeciesCategoryNumber/tot_catch) 
+print(x=df_p_species_per_haul, n=100)
+
+df_pp<-per_length |> full_join(df_p_species_per_haul) |> 
+  mutate(pp=p_per_length*p_species_per_haul) |> 
+  select(pp, everything())
+View(df_pp)
+
+# Check it sums up to 100
+tmp<-df_pp |> group_by(HaulNumber) |> summarise(sumx=sum(x))
+print(tmp, n=50)
 
 
+df_sigma<-df_pp |> mutate(d=9.5325669476e-07) |> # d is the same for all clupeids
+#  mutate(d=ifelse(CatchSpeciesCode==xxxx, 9.1, d) # Example on how to change d for a specific species xxxx
+  mutate(sigma=d*(CatchLengthClass/10+0.2)^2) |>  # sigma=d*(L_cm+offset_cm)^2
+select(sigma, everything())
+View(df_sigma)  
+
+df_sigma_haul<-df_sigma |> mutate(sigma_x_pp=sigma*pp) |> select(sigma_x_pp, everything()) |> 
+  group_by(HaulNumber, HaulStatisticalRectangle) |> 
+  summarise(sigma_haul=sum(sigma_x_pp)/sum(pp)) |> select(sigma_haul, everything())
+View(df_sigma_haul)
+
+df_sigma_rectangle <- df_sigma_haul |> group_by(HaulStatisticalRectangle) |> 
+  summarise(sigma_rectangle=mean(sigma_haul))
+df_sigma_rectangle
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 #  df |> select(rec, LogDistance,DataValue)
 
 # FINAL BIAS excel

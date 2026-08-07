1ef96f273fe36096ebe3ce03cc572a92b6119994
hennip <henni.pulkkinen@luke.fi>
2026-08-07 10:35:47 +0300

    model to cover 2020-2025

--- 03-bayesian-model/model_4species_etaE4etaR4.R
+++ 03-bayesian-model/model_4species_etaE4etaR4.R
@@ -215,11 +215,14 @@ cat(GRAHS_model,file=paste0(modelname,".txt"))
 # inits<-list(list(etaE=array(10000, dim=c(Nspecies, 4, Nyears))),
 #             list(etaE=array(10000, dim=c(Nspecies, 4, Nyears))))
 # 
+model_data<-"_2020-2025"
+run_name<-str_c(modelname, model_data)
 
 data<-list(
-  Nyears=2,
+  Nyears=Nyears,
   Nrec=4,
-  Nages=10,
+  Nages=Nages, # Herring 0 - 8+ yr olds => 9 age groups 
+  
   Nspecies=4,
   Nlengths=c(N_lh,N_lsprat,N_lstickl,N_lo),
   pi=3.14159265358979323846,
@@ -242,7 +245,7 @@ data<-list(
   Lobs=L_obs, # Number of individuals per length group in each sample
   Gobs=G_obs, # Number of individuals per age group in each sample
   nGobs=nG_obs, # sample size per age group
-  aG=rep(1,10),
+  aG=rep(1,Nages),
   aL1=rep(1,N_lh),
   aL2=rep(1,N_lsprat),
   aL3=rep(1,N_lstickl),
@@ -260,8 +263,8 @@ parnames=c(
   "Ntot","N"
 )
 
-sink(paste0("sink_",modelname,"_",".txt"))
-
+sink(paste0("sink_",run_name,"_",".txt"))
+#sink()
 
 t1<-Sys.time();print(t1)
 run1<-run.jags(modelname, monitor=parnames,data=data,n.chains = 2, 
@@ -272,49 +275,49 @@ run1<-run.jags(modelname, monitor=parnames,data=data,n.chains = 2,
                keep.jags.files=F,
                progress.bar=TRUE, jags.refresh=100)
 run<-run1
-save(run, file=paste0(path_output,modelname,".RData"))
+save(run, file=paste0(path_output,run_name,".RData"))
 t2<-Sys.time();print(t2)
 print("run1 done");print(difftime(t2,t1))
 print("--------------------------------------------------")
 
 # 
-plot_this<-F
-if(plot_this==T){
-plot(run, var="etaE")
-plot(run, var="etaR")
-summary(run, var="eta")
-summary(run, var="Ntot")
-summary(run, var="N")
-plot(run, var="Ntot")
-plot(run, var="cv_nasc")
-# chains<-as.mcmc.list(run)
-# chains<-window(chains, start=2000000)
-# traceplot(chains[,"etaE[1]"])
-# traceplot(chains[,"etaE[2]"])
-# traceplot(chains[,"etaE[3]"])
-# traceplot(chains[,"etaE[4]"])
-# summary(chains[,"etaE[1]"])
-sum_run<-summary(run)
+# plot_this<-F
+# if(plot_this==T){
+# plot(run, var="etaE")
+# plot(run, var="etaR")
+# summary(run, var="eta")
+# summary(run, var="Ntot")
+# summary(run, var="N")
+# plot(run, var="Ntot")
+# plot(run, var="cv_nasc")
+# # chains<-as.mcmc.list(run)
+# # chains<-window(chains, start=2000000)
+# # traceplot(chains[,"etaE[1]"])
+# # traceplot(chains[,"etaE[2]"])
+# # traceplot(chains[,"etaE[3]"])
+# # traceplot(chains[,"etaE[4]"])
+# # summary(chains[,"etaE[1]"])
+# sum_run<-summary(run)
+# 
+# as_tibble(sum_run) |> 
+#   filter(psrf>1.1)
+# }
 
-as_tibble(sum_run) |> 
-  filter(psrf>1.1)
-}
-
-run2 <- extend.jags(run1, combine=T, sample=100000, thin=100, keep.jags.files=F)
+run2 <- extend.jags(run1, combine=T, sample=10000, thin=100, keep.jags.files=F)
  t3<-Sys.time();print(t3)
  print("run2 done"); print(difftime(t3,t2))
  print("--------------------------------------------------")
  run<-run2
- save(run, file=paste0(path_output,modelname,".RData"))
+ save(run, file=paste0(path_output,run_name,".RData"))
 
 
  t31<-Sys.time();print(t31)
-run3 <- extend.jags(run2, combine=T, sample=100000, thin=100, keep.jags.files=F)
+run3 <- extend.jags(run2, combine=T, sample=10000, thin=100, keep.jags.files=F)
  t32<-Sys.time();print(t32)
  print("run3 done"); print(difftime(t31,t32))
  print("--------------------------------------------------")
  run<-run3
- save(run, file=paste0(path_output,modelname,".RData"))
+ save(run, file=paste0(path_output,run_name,".RData"))
  
  
 sink()

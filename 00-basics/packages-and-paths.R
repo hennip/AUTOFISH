d62ddde21e81cd25e61b7af827abf83973026338
Pulkkinen Henni (LUKE) <henni.pulkkinen@luke.fi>
2026-08-18 10:50:33 +0300

    rename paths for GRAHS

--- 00-basics/packages-and-paths.R
+++ 00-basics/packages-and-paths.R
@@ -55,8 +55,8 @@ if(computer_name=="es_laptop"){
 }
 
 if(computer_name=="hp_kala1"){
-  pathA_Bayes<-"../../dat/AUTOFISH/orig/Acoustic/"
-  pathB_Bayes<-"../../dat/AUTOFISH/orig/Biotic/"
+  pathA_Bayes<-"../../dat/AUTOFISH/GRAHS/Acoustic/"
+  pathB_Bayes<-"../../dat/AUTOFISH/GRAHS/Biotic/"
   path_output<-"../out/"
 }
 
